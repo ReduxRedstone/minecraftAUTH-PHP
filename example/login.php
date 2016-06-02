b9244576bac071ce92bb99daf31b12bde16f88a1
ReduxRedstone <halolink44@gmail.com>
2016-06-02 08:51:33 -0400

    Added error handling example and result display

--- example/login.php
+++ example/login.php
@@ -4,9 +4,21 @@ require 'mclogin.class.php';
 
 $username = $_POST["username"];
 $password = $_POST["password"];
+$errorHandles = array(
+					1=>"Please enter both an email and password",
+					2=>"Invalid login credentials!",
+					3=>"This is a demo account. Please login with a premium account.",
+					4=>"Unknown error!",
+				);
 
 $auth = new minecraftAUTH();
 
 $result = $auth->login("$username", "$password");
 
-echo "<pre>".print_r($result, true)."</pre>";
+if (!is_array($result)) {
+	echo $errorHandles[$result];
+} else {
+	echo "Successfully logged in as ".$result["userData"]["username"].".";
+}
+
+//echo "<pre>".print_r($result, true)."</pre>";
